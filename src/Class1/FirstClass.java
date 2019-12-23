f5583bfdb96815beffcfa803d6c8c8e3a3dd0f48
talalarshad87 <talalarshad_87@hotmail.com>
2019-12-23 11:58:09 +0500

    some init msg

---
+++ src/Class1/FirstClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class1;
+
+/**
+ *
+ * @author talal
+ */
+public class FirstClass {
+    
+public static void main(String[] args) {   
+System.out.println("This is java Spring53");    
+}
---
+++ src/assign2/TestVariable.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assign2;
+
+
+public class TestVariable {
+    public static void main(String[] args){
+//    //1-decision
+//        boolean a = true;
+//    //2-Character
+//        byte b = '0';// 8 bit
+//        char c = 'A';// 16 bit
+//    //3- Number
+//    //3.1 Whole Number    
+//       short d = 10;// 16bit
+//       int e = 10;// 32bit
+//       long f = 10;// 64 bit
+//    //3,2 Decimals  
+//       float g = 10.0f; // 32 bit       
+//       double h = 10.0; // 64 bit
+//       
+//       short s1 = 100 ,s2=100;
+//       s1 = (short)(s1+s2); //explicit  
+//       f = e + d; //implicit 
+//       e = (int) (e + f);  //explicit
+//       g = f + e; //decimal is considered as big datatype. //Implicit value
+//       h = g + f;// implicit 
+//       
+//     //byte => char => int => long => float => double => short => int //implicit
+//     //====> Implicit
+//     //====> Explicit  
+//    //precision loss is upto developer           
+//       
+       
+     int a,b,c,d,e,f;
+     a=b=c=d=e=g=f=1;
+     
+     a=a+1;// binary addition
+     a +=1; // self addtion
+     a ++; // post increment operator --> after evaluate ++a pre increment -->Before evaluate
+     a --; // decrement operator
+    
+    
+    }        
